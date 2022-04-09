7087e02e1c3fb7c66f892bd16edc8ffcc7594cb6
lsj <a1353474135@163.com>
2022-04-09 12:02:32 +0800

    关注按钮发送数据,商家详情顶部距离,关注按钮样式污染修改

--- src/pages/userinfo/user_follow.vue
+++ src/pages/userinfo/user_follow.vue
@@ -8,7 +8,8 @@
 					<view class="num">在售{{item.sale_num}}件</view>
 				</view>
 				<view class="follow-action">
-					<followButton :follow='item.follow||true' :followID='item.id' @handleSuccess='followSuccess($event,item,index)'></followButton>
+					<followButton :follow='item.follow||true' :followID='item.id'
+						@handleSuccess='followSuccess($event,item,index)'></followButton>
 				</view>
 			</template>
 		</view>
@@ -17,118 +18,128 @@
 </template>
 
 <script lang="ts">
-import { app } from "@/app";
-import { Component } from "vue-property-decorator";
-import BaseNode from "../../base/BaseNode.vue";
-@Component({})
-export default class ClassName extends BaseNode {
-  followList: any = [];
-  totalPage = 0;
-  queryParams = {
-    pageIndex: 1,
-    pageSize: 20
-  };
-  getFollowList() {
-    app.http.Get("me/follows/list", this.queryParams, (res: any) => {
-      this.totalPage = res.totalPage;
-      if (this.queryParams.pageIndex === 1) this.followList = [];
-      const dataList = (res.list || []).map((item: any) => {
-        item.follow = true;
-        return item;
-      });
-      this.followList = [...this.followList, ...dataList];
-    });
-    uni.stopPullDownRefresh();
-  }
-  followSuccess(event: any, item: any, index: number) {
-    this.followList.splice(index, 1);
-  }
-  toDetail(item: any) {
-    uni.navigateTo({
-      url:
-        "/pages/userinfo/merchant_shopsV2?id=" +
-        item.id +
-        "&chaneListKey=followList"
-    });
-  }
+	import {
+		app
+	} from "@/app";
+	import {
+		Component
+	} from "vue-property-decorator";
+	import BaseNode from "../../base/BaseNode.vue";
+	@Component({})
+	export default class ClassName extends BaseNode {
+		followList: any = [];
+		totalPage = 0;
+		queryParams = {
+			pageIndex: 1,
+			pageSize: 20
+		};
+		onLoad() {
+			this.getFollowList();
+			this.onEventUI('followAction', ((res: any) => {
+				if (!res || !res.id) return
+				const findIndex = this.followList.findIndex((item: any) => {
+					return item.id == res.id
+				})
+				//取关
+				if (!res.followed && findIndex >= 0) this.followList.splice(findIndex, 1);
+			}))
+		}
+		onPullDownRefresh() {
+			this.queryParams.pageIndex = 1;
+			this.getFollowList();
+		}
+		onReachBottom() {
+			if (this.queryParams.pageIndex < this.totalPage) {
+				this.queryParams.pageIndex += 1;
+				this.getFollowList();
+			}
+		}
+		getFollowList() {
+			app.http.Get("me/follows/list", this.queryParams, (res: any) => {
+				this.totalPage = res.totalPage;
+				if (this.queryParams.pageIndex === 1) this.followList = [];
+				const dataList = (res.list || []).map((item: any) => {
+					item.follow = true;
+					return item;
+				});
+				this.followList = [...this.followList, ...dataList];
+			});
+			uni.stopPullDownRefresh();
+		}
+		followSuccess(event: any, item: any, index: number) {
+			this.followList.splice(index, 1);
+		}
+		toDetail(item: any) {
+			uni.navigateTo({
+				url: "/pages/userinfo/merchant_shopsV2?id=" + item.id
+			});
+		}
 
-  onLoad() {
-    this.getFollowList();
-  }
-  onPullDownRefresh() {
-    this.queryParams.pageIndex = 1;
-    this.getFollowList();
-  }
-  onReachBottom() {
-    if (this.queryParams.pageIndex < this.totalPage) {
-      this.queryParams.pageIndex += 1;
-      this.getFollowList();
-    }
-  }
-}
+
+	}
 </script>
 
 <style lang='scss'>
-.followContent {
-  padding: 0 41rpx;
-}
+	.followContent {
+		padding: 0 41rpx;
+	}
 
-.follow {
-  margin-bottom: 37rpx;
-  align-items: center;
+	.follow {
+		margin-bottom: 37rpx;
+		align-items: center;
 
-  &-avart {
-    width: 103rpx;
-    height: 103rpx;
-    display: block;
-    border-radius: 50%;
-    overflow: hidden;
-    margin-right: 37rpx;
-  }
+		&-avart {
+			width: 103rpx;
+			height: 103rpx;
+			display: block;
+			border-radius: 50%;
+			overflow: hidden;
+			margin-right: 37rpx;
+		}
 
-  &-info {
-    flex-direction: column;
-    flex: 1;
+		&-info {
+			flex-direction: column;
+			flex: 1;
 
-    .name {
-      font-size: 26rpx;
-      font-family: FZLanTingHeiS-R-GB;
-      font-weight: 400;
-      color: #333333;
-      margin-bottom: 24rpx;
-    }
+			.name {
+				font-size: 26rpx;
+				font-family: FZLanTingHeiS-R-GB;
+				font-weight: 400;
+				color: #333333;
+				margin-bottom: 24rpx;
+			}
 
-    .num {
-      font-size: 22rpx;
-      font-family: FZLanTingHeiS-R-GB;
-      font-weight: 400;
-      color: #88878c;
-    }
-  }
+			.num {
+				font-size: 22rpx;
+				font-family: FZLanTingHeiS-R-GB;
+				font-weight: 400;
+				color: #88878c;
+			}
+		}
 
-  &-action {
-    .actionItem {
-      width: 122rpx;
-      height: 44rpx;
-      font-size: 22rpx;
-      font-family: FZLanTingHeiS-R-GB;
-      font-weight: 400;
-      color: #88878c;
-      line-height: 44rpx;
-      text-align: center;
-      letter-spacing: 1rpx;
-      background-size: 100% 100%;
-      background-image: url("../../static/userinfo/v2/whiteCard.png");
-    }
+		&-action {
+			.actionItem {
+				width: 122rpx;
+				height: 44rpx;
+				font-size: 22rpx;
+				font-family: FZLanTingHeiS-R-GB;
+				font-weight: 400;
+				color: #88878c;
+				line-height: 44rpx;
+				text-align: center;
+				letter-spacing: 1rpx;
+				background-size: 100% 100%;
+				background-image: url("../../static/userinfo/v2/whiteCard.png");
+			}
 
-    .redAction {
-      background-image: url("../../static/userinfo/v2/redCard.png");
-      color: #fff;
-    }
-  }
-}
+			.redAction {
+				background-image: url("../../static/userinfo/v2/redCard.png");
+				color: #fff;
+			}
+		}
+	}
 
-.follow:first-child {
-  margin-top: 56rpx;
-}
+	.follow:first-child {
+		margin-top: 56rpx;
+	}
 </style>
