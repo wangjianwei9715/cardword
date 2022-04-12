558f4219e6d74909893d2f64ea244f940d78267d
lsj <a1353474135@163.com>
2022-04-12 20:20:29 +0800

    跳转地址rest

--- src/pages/userinfo/merchant_list.vue
+++ src/pages/userinfo/merchant_list.vue
@@ -141,8 +141,8 @@
 				return;
 			}
 			// #endif
-			// const path = `/pages/userinfo/merchant_shopsV2`;
-			const path = `/pages/act/loot/index`;
+			const path = `/pages/userinfo/merchant_shopsV2`;
+			// const path = `/pages/act/loot/index`;
 			uni.navigateTo({
 				url: path + "?id=" + item.id
 			});  
