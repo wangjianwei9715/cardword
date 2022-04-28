ad139ef179c7fce79626cabb9427965fdb6f1743
lsj <a1353474135@163.com>
2022-04-28 15:14:25 +0800

    图片删除

--- src/pages/act/teamContest/teamContest_index.vue
+++ src/pages/act/teamContest/teamContest_index.vue
@@ -1,19 +1,41 @@
 <template>
 	<view class="container">
+		<view class="rightFloat" style="top:20rpx;">
+			<text>规则</text>
+		</view>
+		<view class="topImage">
+
+		</view>
+		<view class="topTisp">每次猜对获得2点欧气，每轮比赛进行2场后不可再猜测</view>
 		<view class="contestContainer">
-			<view class="contest-teamCard-container uni-flex" style="margin-bottom: 65rpx;">
+			<view class="contest-teamCard-container uni-flex" style="margin-bottom: 66rpx;">
 				<view class="contest-smallCard">
 					<view class="contest-topScoreBoard uni-flex">
-						<view class="avatar noneAvatar">
+						<view class="avatar selectAvatar">
 						</view>
 						<view class="scoreContent">
-							<view class="scoreContent-left none">0</view>
-							<view class="scoreContent-right none">0</view>
+							<view class="scoreContent-left none red">1</view>
+							<view class="scoreContent-right none blue">1</view>
 						</view>
 						<view class="avatar noneAvatar">
 						</view>
 					</view>
-					<view class="contest-noStart">暂未开始</view>
+					<!-- <view class="contest-noStart">暂未开始</view> -->
+					<!-- <view class="contest-supportButton" @click="modalShow=true">立即支持</view> -->
+					<view class="contest-proportion">
+						<view class="proportionBlock contest-proportion-left">
+						</view>
+						<view class="proportionBlock contest-proportion-right">
+						</view>
+						<view class="support-percent support-percent-left">
+							<text>20%</text>
+							<text>222人支持</text>
+						</view>
+						<view class="support-percent support-percent-right">
+							<text>80%</text>
+							<text>888人支持</text>
+						</view>
+					</view>
 				</view>
 				<view class="contest-smallCard">
 					<view class="contest-topScoreBoard uni-flex">
@@ -26,8 +48,8 @@
 						<view class="avatar noneAvatar">
 						</view>
 					</view>
-					<view class="contest-noStart">暂未开始</view>
-					<!-- <view class="contest-supportButton">立即支持</view> -->
+					<!-- <view class="contest-noStart">暂未开始</view> -->
+					<view class="contest-supportButton" @click="modalShow=true">立即支持</view>
 				</view>
 				<view class="line lineOne"></view>
 			</view>
@@ -108,6 +130,31 @@
 				<view class="line lineThree"></view>
 			</view>
 		</view>
+		<view class="confirmModal" :class="{confirmModalShow:modalShow}">
+			<view class="close" @click="modalShow=false"></view>
+			<view class="tips">请选择您支持的球队</view>
+			<view class="chioceContainer">
+				<view class="teamChoice">篮网</view>
+				<view class="teamChoice">凯尔特人</view>
+			</view>
+		</view>
+		<view class="ruleModal" v-show="ruleShow">
+			<view class="close" @click="ruleShow=false"></view>
+			<view class="rule-title">规则说明</view>
+			<view class="rule-particulars">
+				<text>1.NBA季候赛期间，玩家在活动页面支持自己喜爱的球队就可免费获得欧气</text><br>
+				<text>2.欧气获取：</text><br>
+				<text>1) 首次分享活动页+5欧气数 (限1次)</text>
+			</view>
+		</view>
+		<view class="mask" v-show="modalShow||ruleShow"></view>
+		<view class="bottomContainer">
+			<view class="bottom-button share">
+				<text>分享</text>
+				<text>首次分享欧气+5</text>
+			</view>
+			<view class="bottom-button goLoot" @click="pageJump('/pages/act/loot/index')">前往欧皇夺宝</view>
+		</view>
 	</view>
 </template>
 
@@ -120,7 +167,18 @@
 	import BaseComponent from "@/base/BaseComponent.vue";
 	@Component({})
 	export default class ClassName extends BaseComponent {
-
+		modalShow: boolean = false; 
+		ruleShow: boolean = false;
+		//计算百分比
+		caclPercent(){
+			
+		}
+		pageJump(url: string) {
+			if (!url) return
+			uni.navigateTo({
+				url
+			})
+		}
 	}
 </script>
 
@@ -134,16 +192,116 @@
 		height: 1334rpx;
 		background-size: 100% 100%;
 		background-image: url(../../../static/act/teamContest/back_img.png);
+
+		.rightFloat {
+			width: 100rpx;
+			height: 96rpx;
+			position: absolute;
+			right: 4rpx;
+			background-size: 100% 100%;
+			background-image: url(../../../static/act/goldRank/active_rule.png);
+			font-size: 20rpx;
+			font-family: Alibaba PuHuiTi;
+			font-weight: 500;
+			color: #FFFFFF;
+			text-align: center;
+			letter-spacing: 2rpx;
+			line-height: 96rpx;
+
+			text {
+				/* position: relative;
+				top: 30rpx; */
+			}
+		}
 	}
 
+	.topImage {
+		width: 680rpx;
+		height: 210rpx;
+		background-size: 100% 100%;
+		position: relative;
+		background-image: url(../../../static/act/teamContest/top.png);
+		position: relative;
+		right: 36rpx;
 
+	}
+
+	.topTisp {
+		text-align: center;
+		font-size: 20rpx;
+		font-family: PingFang SC;
+		font-weight: 600;
+		color: #FFFFFF;
+		position: relative;
+		letter-spacing: 2rpx;
+		margin-bottom: 13rpx;
+	}
 
 	.contestContainer {
-		padding: 0 47rpx;
-		padding-top: 240rpx;
+		/* padding: 0 47rpx; */
+		width:660rpx ;
+		margin: 0 auto;
 		position: relative;
-		padding-bottom: calc(134rpx + constant(safe-area-inset-bottom));
-		padding-bottom: calc(134rpx + env(safe-area-inset-bottom));
+		padding-bottom: calc(140rpx + constant(safe-area-inset-bottom));
+		padding-bottom: calc(140rpx + env(safe-area-inset-bottom));
+	}
+
+	.bottomContainer {
+		width: 750rpx;
+		background-size: 100% 100%;
+		position: fixed;
+		bottom: 0;
+		background-image: url(../../../static/act/teamContest/white_bottom.png);
+		height: calc(124rpx + constant(safe-area-inset-bottom));
+		height: calc(124rpx + env(safe-area-inset-bottom));
+		z-index: 3;
+		display: flex;
+		justify-content: center;
+
+		.bottom-button {
+			width: 343rpx;
+			height: 78rpx;
+			border-radius: 3rpx;
+			letter-spacing: 2rpx;
+			margin-top: 22rpx;
+		}
+
+		.share {
+			border: 2rpx solid #DADADA;
+			margin-right: 22rpx;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			flex-direction: column;
+
+			text {
+				font-family: Alibaba PuHuiTi;
+				color: #5E5D5D;
+				font-weight: 400;
+				display: block;
+				width: 100%;
+				text-align: center;
+			}
+
+			text:first-child {
+				font-size: 31rpx;
+			}
+
+			text:last-child {
+				font-size: 21rpx;
+			}
+
+		}
+
+		.goLoot {
+			background: #EF3333;
+			font-size: 33rpx;
+			font-family: Alibaba PuHuiTi;
+			font-weight: 400;
+			color: #FFFFFF;
+			text-align: center;
+			line-height: 78rpx;
+		}
 	}
 
 	.contest-teamCard-container {
@@ -177,7 +335,8 @@
 			background-image: url(../../../static/act/teamContest/lineTwo.png);
 			position: absolute;
 		}
-		.lineThree{
+
+		.lineThree {
 			width: 408rpx;
 			height: 102rpx;
 			background-image: url(../../../static/act/teamContest/lineThree.png);
@@ -233,13 +392,13 @@
 			height: 59rpx;
 			background: #B7B7B7;
 			border-radius: 50%;
-			overflow: hidden;
+
 			display: block;
 			position: relative;
 		}
 
 		.noneAvatar::after {
-			content: ' ';
+			content: '';
 			display: block;
 			width: 29rpx;
 			height: 40rpx;
@@ -251,6 +410,19 @@
 			margin: auto;
 			background-size: 100% 100%;
 			background-image: url(../../../static/act/teamContest/smallVs.png);
+		}
+
+		.selectAvatar::after {
+			content: '';
+			display: block;
+			width: 68rpx;
+			height: 68rpx;
+			border: 2rpx solid #EF3333;
+			border-radius: 50%;
+			position: absolute;
+			left: 50%;
+			top: 50%;
+			transform: translate(-50%, -50%);
 		}
 
 		.scoreContent {
@@ -274,6 +446,14 @@
 
 			.none {
 				color: #333333;
+			}
+
+			.red {
+				color: #EF3333;
+			}
+
+			.blue {
+				color: #245BE7;
 			}
 		}
 
@@ -319,6 +499,202 @@
 		font-weight: 400;
 		color: #595959;
 		letter-spacing: 2rpx;
+	}
+
+	.contest-proportion {
+		width: 100%;
+		height: 53rpx;
+		background-color: #fff;
+		overflow: hidden;
+		display: flex;
+
+		.proportionBlock {
+			height: 53rpx;
+			position: absolute;
+		}
+
+		&-left {
+			background-color: #ff0023;
+			left: 0;
+			text-align: left;
+			width:calc(50% - 18rpx);
+			z-index: 2;
+
+		}
+
+		&-left::after {
+			content: '';
+			display: block;
+			width: 0;
+			position: absolute;
+			height: 0;
+			border-color: #ff0023 transparent transparent transparent;
+			border-style: solid;
+			border-width: 53rpx 36rpx 0 0;
+			right: -35rpx;
+			top: 0;
+			z-index: 1;
+		}
+
+		&-right {
+			background-color: #005df0;
+			right: 0;
+			width: calc(50% - 18rpx);
+			text-align: right;
+
+			text {
+				margin-right: 10rpx;
+			}
+		}
+
+		&-right::after {
+			content: '';
+			display: block;
+			width: 0;
+			position: absolute;
+			height: 0;
+			border-color: transparent #005df0 transparent transparent;
+			border-style: solid;
+			border-width: 53rpx 36rpx 0 0;
+			left: -35rpx;
+		}
+
+		.support-percent {
+			position: absolute;
+			width: 50%;
+			font-size: 20rpx;
+			font-family: PingFang SC;
+			font-weight: 600;
+			color: #FFFFFF;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			height: 53rpx;
+			z-index: 3;
+			position: absolute;
+			white-space: nowrap;
+			line-height: 24rpx;
+		}
+
+		.support-percent-left {
+			text-align: left;
+			left: 14rpx;
+		}
+
+		.support-percent-right {
+			text-align: right;
+			right: 14rpx;
+		}
+	}
+
+	.mask {
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		background-color: rgba(0, 0, 0, 0.5);
+		z-index: 8;
+	}
+
+	.confirmModal {
+		width: 516rpx;
+		height: 317rpx;
+		background: #FFFFFF;
+		border-radius: 5rpx;
+		position: fixed;
+		left: 0;
+		right: 0;
+		margin: auto;
+		top: 447rpx;
+		z-index: 9;
+		opacity: 0;
+		transform: scale(0);
+		transition: all 0.3s;
+
+		.close {
+			width: 36rpx;
+			height: 33rpx;
+			background-size: 100% 100%;
+			background-image: url(../../../static/act/teamContest/close.png);
+			position: absolute;
+			top: -75rpx;
+			right: 0;
+		}
+
+		.tips {
+			text-align: center;
+			font-size: 33rpx;
+			font-family: PingFang SC;
+			font-weight: 400;
+			color: #333333;
+			margin-top: 89rpx;
+		}
+
+		.chioceContainer {
+			padding: 0 27rpx;
+			display: flex;
+			justify-content: space-between;
+			margin-top: 57rpx;
+
+			.teamChoice {
+				width: 221rpx;
+				height: 65rpx;
+				background: #E13737;
+				border-radius: 3rpx;
+				text-align: center;
+				font-size: 29rpx;
+				font-family: FZLanTingHeiS-R-GB;
+				font-weight: 400;
+				color: #FFFFFF;
+				letter-spacing: 2rpx;
+				line-height: 65rpx;
+			}
+		}
+	}
+
+	.confirmModalShow {
+		opacity: 1;
+		transform: scale(1);
+	}
+
+	.ruleModal {
+		width: 750rpx;
+		height: calc(950rpx + constant(safe-area-inset-bottom));
+		height: calc(950rpx + env(safe-area-inset-bottom));
+		background: #FFFFFF;
+		border-radius: 5rpx 5rpx 0px 0px;
+		position: fixed;
+		bottom: 0;
+		z-index: 9;
+
+		.close {
+			width: 29rpx;
+			height: 27rpx;
+			background-size: 100% 100%;
+			background-image: url(../../../static/act/teamContest/black_close.png);
+			position: absolute;
+			top: 23rpx;
+			right: 23rpx;
+		}
+
+		.rule-title {
+			font-size: 42rpx;
+			font-family: PingFang SC;
+			font-weight: 600;
+			color: #333333;
+			margin: 30rpx 0;
+			text-align: center;
+		}
+
+		.rule-particulars {
+			padding: 0 35rpx;
+			font-size: 25rpx;
+			font-family: PingFang SC;
+			font-weight: 400;
+			color: #7D8288;
+
+		}
 	}
 
 	.just-center {
