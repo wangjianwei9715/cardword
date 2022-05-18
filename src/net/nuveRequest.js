514aa863ce2a7f65cfd5bf328631caf4e38931bb
lsj <a1353474135@163.com>
2022-05-18 17:38:45 +0800

    直播nvue页面,我的直播列表页面,nvueRequest请求

---
+++ src/net/nuveRequest.js
@@ -0,0 +1,33 @@
+const request = (url, params = {}, method = 'GET') => {
+	const isTest = false
+	const requestUrl = isTest ? 'http://192.168.8.80:8701/api/v2/' : 'https://server.ssl1.ka-world.com/api/v2/'
+	return new Promise((resolve, reject) => {
+		const httpReg = /^http(s)?/
+		const finUrl = httpReg.test(url) ? url : requestUrl + url
+		console.log(finUrl);
+		uni.request({
+			url: finUrl,
+			data: params,
+			header: {
+				token: uni.getStorageSync('token') && JSON.parse(uni.getStorageSync('token'))
+					.accessToken
+			},
+			method: method.toUpperCase(),
+			success: (res) => {
+				if (res.data.code != 0) {
+					uni.showToast({
+						title: res.data.msg,
+						icon: 'none'
+					})
+					reject(res.data.msg)
+					return
+				}
+				resolve(res.data)
+			},
+			fail: (err) => {
+				reject(err)
+			}
+		})
+	})
+}
+export default request
---
+++ src/pages/live/myLive.vue
@@ -0,0 +1,123 @@
+<template>
+	<view>
+		<view class="liveItem">
+			<view class="liveItem-top uni-flex">
+				<image class='avatar'></image>
+				<view class="name">卡皇球星社</view>
+			</view>
+			<view class="liveItem-message uni-flex">
+				<image class="goodsImage"></image>
+				<view class="live-right">
+					<view>
+						<view class='goodsName'>20-21 panini prizm prizm...</view>
+						<view class='startTime'>开播时间：04-05 11:11:11</view>
+					</view>
+					<view class='uni-flex' style='justify-content: space-between;align-items:center'>
+						<view class='state'>待直播</view>
+						<view class='startButton'>进入直播间</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script lang="ts">
+	import {
+		app
+	} from "@/app";
+	import {
+		Component
+	} from "vue-property-decorator";
+	import BaseNode from '../../base/BaseNode.vue';
+	@Component({})
+	export default class ClassName extends BaseNode {
+		dataList: any = []
+		onLoad() {
+
+		}
+	}
+</script>
+
+<style lang='scss'>
+	.liveItem {
+		padding: 0 26rpx;
+		margin-top: 33rpx;
+	}
+
+	.liveItem-top {
+		align-items: center;
+
+		.avatar {
+			width: 48rpx;
+			height: 48rpx;
+			background: #D9D8DB;
+			display: block;
+			border-radius: 50%;
+		}
+
+		.name {
+			font-size: 25rpx;
+			font-family: PingFang SC;
+			font-weight: 600;
+			color: #333333;
+			margin-left: 17rpx;
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
+	}
+
+	.liveItem-message {
+		margin-top: 13rpx;
+		justify-content: space-between;
+
+		.goodsImage {
+			width: 188rpx;
+			height: 188rpx;
+			background: #333333;
+			border-radius: 3rpx;
+			display: block;
+		}
+
+		.live-right {
+			width: 69%;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+		}
+
+		.goodsName {
+			font-size: 29rpx;
+			font-family: PingFang SC;
+			font-weight: 500;
+			color: #333333;
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
+
+		.startTime {
+			font-size: 25rpx;
+			font-family: PingFang SC;
+			font-weight: 400;
+			color: #88878C;
+			margin-top: 18rpx;
+		}
+		.startButton{
+			width: 161rpx;
+			height: 52rpx;
+			background: #F5162B;
+			border-radius: 3rpx;
+			text-align:center;
+			line-height:52rpx;
+			color:#fff;
+		}
+		.state{
+			font-size: 25rpx;
+			font-family: PingFang SC;
+			font-weight: 600;
+			color: #333333;
+		}
+	}
+</style>
