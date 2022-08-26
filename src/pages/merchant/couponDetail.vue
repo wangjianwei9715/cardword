2e5c47d291d0a49a959ed743f3a333b33207c1d8
lsj <a1353474135@163.com>
2022-08-26 09:20:50 +0800

    下线优惠券

--- src/pages/merchant/couponDetail.vue
+++ src/pages/merchant/couponDetail.vue
@@ -1,27 +1,28 @@
 <template>
     <view class='content'>
-        <navigationShare :navigatetoTitle="isGoods?'指定商品券':'店铺通用券'" rightText='新建' @onClickRightText="onClickRightText" />
+        <navigationShare :navigatetoTitle="isGoods?'指定商品券':'店铺通用券'" rightText='新建'
+            @onClickRightText="onClickRightText" />
         <view style="padding-top:88rpx">
             <statusbar />
         </view>
         <view class="goodsContainer"></view>
-        <u-sticky :customNavHeight="88">
+        <u-sticky :offsetTop="app.statusBarHeight+44">
             <view class="tagContainr"></view>
         </u-sticky>
         <view class="couponContainer">
 
         </view>
-        <merchantCoupon :showDrawer.sync="createCouponShow"/>
+        <merchantCoupon :showDrawer.sync="createCouponShow" />
     </view>
 </template>
 
 <script lang="ts">
-    interface Tag{
-        index:number,
-        list:Array<ListItem>
+    interface Tag {
+        index: number,
+        list: Array<ListItem>
     }
     interface ListItem {
-        label:string
+        label: string
     }
     import { app } from "@/app";
     import { Component, Watch } from "vue-property-decorator";
@@ -30,31 +31,51 @@
     @Component({})
     export default class ClassName extends BaseNode {
         parsePic: any = parsePic;
-        isGoods:boolean=true
-        tag:Tag={
-            index:0,
-            list:[{label:'全部'},{label:'线上优惠券'},{label:'兑换码'}]
+        app:any=app
+        isGoods: boolean = true
+        tag: Tag = {
+            index: 0,
+            list: [{ label: '全部' }, { label: '线上优惠券' }, { label: '兑换码' }]
         }
-        
-        createCouponShow:boolean=false;
-        onClickRightText(){
-            this.createCouponShow=true
+        createCouponShow: boolean = false;
+
+        onOffLineClick() {
+            uni.showModal({
+                title: '提示',
+                content: '是否下线该优惠券?',
+                success: ({ confirm, cancel }) => {
+                    confirm && this.offLineAction()
+                }
+            })
+        }
+        offLineAction() {
+            console.log('下线');
+        }
+        setClipboardData(data: string) {
+            uni.setClipboardData({
+                data,
+            })
+        }
+        onClickRightText() {
+            this.createCouponShow = true
         }
     }
 </script>
 
 <style lang="scss">
-    .goodsContainer{
+    .goodsContainer {
         width: 750rpx;
         background-color: blue;
         height: 200rpx;
     }
-    .tagContainr{
+
+    .tagContainr {
         width: 750rpx;
         height: 100rpx;
         background-color: red;
     }
-    .couponContainer{
+
+    .couponContainer {
         width: 750rpx;
         height: 10000rpx;
     }
