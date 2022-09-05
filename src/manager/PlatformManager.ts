f0165b290a574989093ce5c49743e54b6cae6816
lsj <a1353474135@163.com>
2022-09-05 15:15:27 +0800

    调整goMerchantPage

--- src/manager/PlatformManager.ts
+++ src/manager/PlatformManager.ts
@@ -181,17 +181,12 @@ export default class PlatformManager {
 	}
 	//跳转商家中心
 	goMerchantPage(alias:string,isMerchant?:boolean){
-		const TO_NEW_MERCHANT_PAGE=false
-		if(TO_NEW_MERCHANT_PAGE){
-			uni.navigateTo({
-				url:`/pages/merchant/core?alias=${alias}${isMerchant?'&isMerchant=true':''}`
-			})
-		}else{
-			uni.navigateTo({
-				url:`/pages/userinfo/merchant_shopsV2?alias=${alias}${isMerchant?'&isMerchant=true':''}`
-			})
-		}
-		
+		if(!alias) return
+		const TO_NEW_MERCHANT_PAGE=true//是否跳转新商家中心
+		const path=TO_NEW_MERCHANT_PAGE?`/pages/merchant/core`:`/pages/userinfo/merchant_shopsV2`
+		uni.navigateTo({
+			url:`${path}?alias=${alias}${isMerchant?'&isMerchant=true':''}`
+		})
 	}
 	requestSubscribeMessage(id: string, callback?: Function) {
 		// 调起订阅消息
