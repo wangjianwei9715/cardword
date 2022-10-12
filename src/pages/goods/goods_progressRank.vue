de2e642f7e4229042950b6587eac0ff144bd01f7
lsj <a1353474135@163.com>
2022-10-12 15:30:03 +0800

    拼团进度榜单

---
+++ src/pages/goods/goods_progressRank.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="content">
+        <view class="topBanner"></view>
+	</view>
+</template>
+
+<script lang="ts">
+	import { app } from "@/app";
+	import { Component } from "vue-property-decorator";
+	import BaseNode from '../../base/BaseNode.vue';
+	@Component({})
+	export default class ClassName extends BaseNode {
+
+		onLoad(query:any) {
+			
+		}
+        onReachBottom(){
+
+        }
+        onPulldDownRefresh(){
+            
+        }
+
+	}
+</script>
+
+<style lang="scss">
+.topBanner{
+    width: 750rpx;
+    height: 350rpx;
+    background-size: 100% 100%;
+    background-image: url('../../static/goods/v2/progressRank.png');
+}
+</style>
