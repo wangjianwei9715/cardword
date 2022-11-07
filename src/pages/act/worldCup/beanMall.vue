1925ccbd82fb300e0842c56376eee0b7980a40f9
lsj <a1353474135@163.com>
2022-11-07 17:42:39 +0800

    活动页面

---
+++ src/pages/act/worldCup/beanMall.vue
@@ -0,0 +1,47 @@
+<template>
+    <view class="content">
+    </view>
+</template>
+
+<script lang="ts">
+import { app } from "@/app";
+import { Component } from "vue-property-decorator";
+import BaseNode from '@/base/BaseNode.vue';
+@Component({})
+export default class ClassName extends BaseNode {
+    queryParams: any = {
+        pageIndex: 1,
+        pageSize: 20
+    }
+    list: any = []
+    totalPage: number = 0
+    onLoad(query: any) {
+        this.reqNewData()
+    }
+    onReachBottom() {
+        if (this.queryParams.pageIndex < this.totalPage) {
+            this.queryParams.pageIndex += 1
+            this.reqNewData()
+        }
+    }
+    onPulldDownRefresh() {
+        this.queryParams.pageIndex = 1
+        this.reqNewData(() => {
+            uni.stopPullDownRefresh()
+        })
+    }
+    reqNewData(cb?: any) {
+        app.http.Get(`dataApi`, this.queryParams, (res: any) => {
+            const list = res.list || []
+            this.totalPage = res.totalPage
+            this.queryParams.pageIndex == 1 ? this.list = list : this.list.push(...list)
+            cb && cb()
+        })
+    }
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/pages/act/worldCup/draw.vue
@@ -0,0 +1,25 @@
+<template>
+    <view class="content">
+    </view>
+</template>
+
+<script lang="ts">
+import { app } from "@/app";
+import { Component } from "vue-property-decorator";
+import BaseNode from '@/base/BaseNode.vue';
+@Component({})
+export default class ClassName extends BaseNode {
+    onLoad(query: any) {
+    }
+    reqNewData(cb?: any) {
+        app.http.Get(`dataApi`, {}, (res: any) => {
+           
+        })
+    }
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
