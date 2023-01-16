e6ed34d5b5142e08096eb3bc260a5bdf34712922
lsj <a1353474135@163.com>
2023-01-16 10:03:08 +0800

    字段修改

--- src/pages/act/imm/index.vue
+++ src/pages/act/imm/index.vue
@@ -2,7 +2,7 @@
  * @Author: lsj a1353474135@163.com
  * @Date: 2023-01-10 15:11:49
  * @LastEditors: lsj a1353474135@163.com
- * @LastEditTime: 2023-01-11 16:36:28
+ * @LastEditTime: 2023-01-16 10:02:18
  * @FilePath: \card-world\src\pages\act\imm\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -98,7 +98,7 @@
                 </view>
                 <template v-if="rankTag.index == 0">
                     <muqian-lazyLoad class="rank_reward" borderRadius="3rpx" v-if="myRank.isPass"
-                        :src="$parsePic(decodeURIComponent(myRank.awardPic_url))" />
+                        :src="$parsePic(decodeURIComponent(myRank.awardPic))" />
                     <view class="rank_normal" v-if="!myRank.isPass">距离入榜还需<br>{{ myRank.passScore || '获取中' }}积分</view>
                 </template>
                 <template v-if="rankTag.index == 1">
