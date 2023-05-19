637ba8c91ad2be3927258711954e1c7f22db384c
lsj <a1353474135@163.com>
2023-05-19 16:58:54 +0800

    1

--- src/pages/act/merchantRank/shuke/index.vue
+++ src/pages/act/merchantRank/shuke/index.vue
@@ -2,7 +2,7 @@
  * @Author: lsj a1353474135@163.com
  * @Date: 2022-11-07 17:20:31
  * @LastEditors: lsj a1353474135@163.com
- * @LastEditTime: 2023-05-19 16:15:59
+ * @LastEditTime: 2023-05-19 16:58:10
  * @FilePath: \jichao_app_2\src\pages\act\worldCup\rank.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -266,7 +266,7 @@ page {
         position: absolute;
         top: 59rpx;
         right: 0;
-
+        z-index: 99;
         .ruleBlock {
             width: 100rpx;
             height: 90rpx;
