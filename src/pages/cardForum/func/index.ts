b1d49e84e997e290689d2491df272ca16df40bb9
lsj <a1353474135@163.com>
2023-06-25 19:53:09 +0800

    卡册，动态草稿箱存储,获取

--- src/pages/cardForum/func/index.ts
+++ src/pages/cardForum/func/index.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: lsj a1353474135@163.com
+ * @Date: 2023-06-19 18:05:04
+ * @LastEditors: lsj a1353474135@163.com
+ * @LastEditTime: 2023-06-25 19:47:46
+ * @FilePath: \card-world\src\pages\cardForum\func\index.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { app } from "@/app"
 import CardForum from "../interface/public"
 export enum CardForumReportSource {
@@ -46,5 +54,54 @@ export function reportAction(form: Report): Promise<Boolean> {
         }, (err: any) => {
             reject(false)
         })
+    })
+}
+function getStamp() {
+    return Math.round(+new Date() / 1000)
+}
+const DRAFT_STORAGE_KEY = "cardForum_draft"
+//获取草稿箱
+export function getDraftList(type: ("cardBook" | "dynamic" | "all")): Array<any> {
+    let list = uni.getStorageSync(DRAFT_STORAGE_KEY) || []
+    if (!list || !list.length) return []
+    if (type !== "all") {
+        list = list.filter((item: any) => {
+            return item.type == type
+        })
+    }
+    return list.sort((x: any, y: any) => {
+        return y.stamp - x.stamp
+    })
+}
+//存储草稿箱
+export function storageDraft(data: any, type: ("cardBook" | "dynamic"), draftId?: string): Promise<Boolean> {
+    return new Promise((resovle, reject) => {
+        let beforeList = getDraftList(type)
+        let saveData: any = {
+            stamp: getStamp(),
+            type,
+            data,
+        }
+        if (!draftId) {
+            saveData.draftId = uni.$u.guid(4)
+            beforeList.unshift(saveData)
+            uni.setStorageSync(DRAFT_STORAGE_KEY, beforeList)
+            resovle(true)
+        } else {
+            const findIndex = beforeList.findIndex((item: any) => {
+                return item.draftId === draftId
+            })
+            if (findIndex >= 0) {
+                beforeList[findIndex].stamp = getStamp()
+                beforeList[findIndex].data = data
+            } else {
+                saveData.draftId = uni.$u.guid(4)
+            }
+            beforeList = beforeList.sort((x: any, y: any) => {
+                return y.stamp - x.stamp
+            })
+            uni.setStorageSync(DRAFT_STORAGE_KEY, beforeList)
+            resovle(true)
+        }
     })
 }
