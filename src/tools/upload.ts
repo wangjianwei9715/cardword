d28b2ea0e9aae888e6aad930928e6870768f2c89
lsj <a1353474135@163.com>
2023-06-27 17:16:20 +0800

    文件名调整

--- src/tools/upload.ts
+++ src/tools/upload.ts
@@ -10,12 +10,12 @@ export default class Upload {
         }
         return Upload.instance
     }
-    uploadFile(file: any, sign: any, fileDir = "") {
+    uploadFile(file: any, sign: any, fileDir = "", type = "images") {
         return new Promise((resolve, reject) => {
             const fileList = file.tempFilePaths;
             let arr: any = []
             for (let i = 0; i < fileList.length; i++) {
-                const fileName = "images/app/" + fileDir + this.ossutils.getFileName(file.tempFiles[i]); // 自定义上传后的文件名称
+                const fileName = type + "/app/" + fileDir + this.ossutils.getFileName(file.tempFiles[i]); // 自定义上传后的文件名称
                 uni.uploadFile({
                     url: sign.host,
                     filePath: fileList[i],
@@ -51,7 +51,7 @@ export default class Upload {
             }
         })
     }
-    getImages(count = 1,fileDir:string, sourceType = ['album']) {
+    getImages(count = 1, fileDir: string, sourceType = ['album']) {
         return new Promise((resolve, reject) => {
             uni.chooseImage({
                 sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
@@ -72,14 +72,22 @@ export default class Upload {
             })
         });
     }
+    getName(filePath: string | any) {
+        return filePath.substring(filePath.lastIndexOf('/') + 1);
+    }
     async upLoadImagePath(...params: any) {
         try {
             //@ts-ignore
-            const fileRes:any = await this.getImages(...params); // 选择图片
+            const fileRes: any = await this.getImages(...params); // 选择图片
             const sign = await this.ossutils.getSTS(); // 获取签名等信息
             uni.showLoading({
                 title: '上传图片中...'
             });
+            // #ifdef APP-PLUS
+            fileRes.tempFilePaths.forEach((path:any,index:number) => {
+                fileRes.tempFiles[index].name=this.getName(path)
+            });
+            // #endif
             const imgUrls = await this.uploadFile(fileRes, sign, params[1])
             return imgUrls
         } catch (err) {
@@ -94,27 +102,43 @@ export default class Upload {
     }
     async upLoadVideoPath(fileDir = "") {
         try {
-            let res: any = await uni.chooseMedia({
-                count: 1,
-                mediaType: ['video'],
+            let res: any = await uni.chooseVideo({
                 sourceType: ["album"],
             })
             res = res.filter(Boolean)
-            if (res[0].errMsg == "chooseMedia:fail cancel") throw new Error("取消")
+            if (res[0].errMsg == "chooseVideo:fail cancel") throw new Error("取消")
+            if (res[0].duration < 5) throw new Error("不能上传时长小于5秒的视频")
             const sign = await this.ossutils.getSTS(); // 获取签名等信息
             uni.showLoading({
                 title: '上传中...'
             });
-            const path: any = await this.uploadFile([res[0].tempFiles[0].tempFilePath], sign, fileDir)
+            let path: any
+            // #ifdef APP-PLUS
+            path = await this.uploadFile({
+                tempFilePaths: [res[0].tempFilePath],
+                tempFiles: [{ name: this.getName(res[0].tempFilePath) }]
+            }, sign, fileDir, "video")
+            // #endif
+            // #ifdef H5
+            path = await this.uploadFile({
+                ...res[0],
+                tempFilePaths: [res[0].tempFilePath],
+                tempFiles: [res[0].tempFile]
+            }, sign, fileDir, "video")
+            // #endif
             uni.hideLoading()
-            return path[0]
+            return {
+                path: path[0],
+                size: res[0].size,
+                duration: res[0].duration
+            }
         } catch (err) {
             uni.hideLoading()
         }
     }
     async uploadImgs(count: number, fileDir: string, sourceType = ['album']) {
-            const result = await this.upLoadImagePath(count, fileDir, sourceType);
-            return result;
+        const result = await this.upLoadImagePath(count, fileDir, sourceType);
+        return result;
     }
     async uploadVideo(fileDir = "") {
         const result = await this.upLoadVideoPath(fileDir);
