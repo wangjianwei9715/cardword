e1e34ad029dbf68326ce1336f04e6f906e32a579
lsj <a1353474135@163.com>
2023-07-05 11:41:10 +0800

    1

--- src/pages/cardForum/components/waterfalls.vue
+++ src/pages/cardForum/components/waterfalls.vue
@@ -2,7 +2,7 @@
  * @Author: lsj a1353474135@163.com
  * @Date: 2023-06-13 11:25:59
  * @LastEditors: lsj a1353474135@163.com
- * @LastEditTime: 2023-07-05 11:32:28
+ * @LastEditTime: 2023-07-05 11:41:01
  * @FilePath: \card-world\src\pages\cardForum\components\waterfalls.vue
  * @Description: 瀑布流
 -->
@@ -494,7 +494,7 @@ export default {
             })
         },
         goToUserProfile(event, item) {
-            if (this.type == "draftList") return
+            if (this.type == "draftList" || !this.showUser) return
             // #ifdef APP-NVUE
             event.stopPropagation();
             // #endif
@@ -756,14 +756,14 @@ export default {
             const { index, list } = this.findIndex(res.code)
             // #ifndef APP-NVUE
             if (index >= 0) {
-                this[list][index].title=res.formData.title
-                this[list][index].cover=res.formData.cover
+                this[list][index].title = res.formData.title
+                this[list][index].cover = res.formData.cover
             }
             // #endif
             // #ifdef APP-NVUE
             if (index >= 0) {
-                this.tempList[index].title=res.formData.title
-                this.tempList[index].cover=res.formData.cover
+                this.tempList[index].title = res.formData.title
+                this.tempList[index].cover = res.formData.cover
             }
             // #endif
         }
