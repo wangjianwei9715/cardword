f747b02e6f7b2ae2ad3a1edc0a849548fb9a11a0
lsj <a1353474135@163.com>
2023-07-05 10:47:43 +0800

    1

--- src/pages/cardForum/components/waterfalls.vue
+++ src/pages/cardForum/components/waterfalls.vue
@@ -2,7 +2,7 @@
  * @Author: lsj a1353474135@163.com
  * @Date: 2023-06-13 11:25:59
  * @LastEditors: lsj a1353474135@163.com
- * @LastEditTime: 2023-07-05 10:45:53
+ * @LastEditTime: 2023-07-05 10:47:39
  * @FilePath: \card-world\src\pages\cardForum\components\waterfalls.vue
  * @Description: 瀑布流
 -->
@@ -436,7 +436,7 @@ export default {
         // #ifdef APP-NVUE
         this.safeBottomHeight = plus.navigator.getSafeAreaInsets().deviceBottom
         // #endif
-        uni.$on("")
+        // uni.$on("")
     },
     methods: {
         onrefresh() {
