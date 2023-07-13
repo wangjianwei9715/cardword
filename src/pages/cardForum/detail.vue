33a8fbd6202b29b5a00c7b294168c376c4e48e95
lsj <a1353474135@163.com>
2023-07-13 14:13:27 +0800

    调整

--- src/pages/cardForum/detail.vue
+++ src/pages/cardForum/detail.vue
@@ -3,7 +3,7 @@
  * @Author: lsj a1353474135@163.com
  * @Date: 2023-06-12 16:06:41
  * @LastEditors: lsj a1353474135@163.com
- * @LastEditTime: 2023-07-13 10:49:35
+ * @LastEditTime: 2023-07-13 14:12:56
  * @FilePath: \jichao_app_2\src\pages\cardForum\detail.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -178,7 +178,7 @@
                 <!-- <view class="line" v-if="index + 1 < commList.length"></view> -->
             </view>
         </view>
-        <u-loadmore style="margin-top: 20rpx;" fontSize="24rpx" v-if="commList && commList.length"
+        <u-loadmore lineLength="20rpx" style="margin-top: 20rpx;" fontSize="24rpx" v-if="commList && commList.length"
             :status="isFetchEnd ? 'nomore' : 'loading'" line />
         <view class="fixInputContainer">
             <view class="wrap">
