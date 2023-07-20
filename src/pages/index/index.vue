e12e1710c8584e8dbc2026f72267c51e71104169
王健伟 <849355894@qq.com>
2023-07-20 15:10:51 +0800

    1

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,7 @@
  * @Author: wjw
  * @Date: 2023-07-03 11:32:48
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-07-20 11:33:39
+ * @LastEditTime: 2023-07-20 15:10:11
  * Copyright: 2023 .
  * @Descripttion: 
 -->
@@ -12,20 +12,20 @@
 		<!-- 热更新 S -->
 		<update/>
 		<!-- 热更新 E -->
-		<view class="header-banner">
-			<statusbar />
-			<view class="tab-header">
-				<view class="header-search">
-					<view class="sousuo-icon"></view>
-					<u-notice-bar style="padding-left:80rpx;" @click="onClickSearch" :text="noticeList" direction="column" icon="" color="#A3A3A3" bgColor="rgba(0,0,0,0)" :duration="5000"></u-notice-bar>
-					<view class="search-btn">搜索</view>
+		<u-sticky>
+			<view class="header-banner">
+				<statusbar />
+				<view class="tab-header">
+					<view class="header-search">
+						<view class="sousuo-icon"></view>
+						<u-notice-bar style="padding-left:80rpx;" @click="onClickSearch" :text="noticeList" direction="column" icon="" color="#A3A3A3" bgColor="rgba(0,0,0,0)" :duration="5000"></u-notice-bar>
+						<view class="search-btn">搜索</view>
+					</view>
+					<navigator url="/pages/live/list" hover-class="none" class="icon-live"></navigator>
 				</view>
-				<navigator url="/pages/live/list" hover-class="none" class="icon-live"></navigator>
 			</view>
-		</view>
+		</u-sticky>
 		<view class="tab-center">
-			<statusbar style="background:#fff" />
-			<view class="center-top"></view>
 			<view class="capsule-box" @click="onClickAddJump(item.target)" v-for="(item,index) in addList.top" :key="index">
 				<image class="capsule-pic1" :src="decodeURIComponent(item.pic)" mode="aspectFill"/>
 			</view>
@@ -328,7 +328,7 @@
 	.header-banner {
 		width: 100%;
 		background: #fff;
-		position: fixed;
+		position: relative;
 		left: 0;
 		top: 0;
 		box-sizing: border-box;
