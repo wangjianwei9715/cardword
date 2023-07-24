b35d21db1ddd830e374b4ea0378a20e9fa380f2f
lsj <a1353474135@163.com>
2023-07-24 14:01:02 +0800

    1

--- src/pages/cardForum/draftList.vue
+++ src/pages/cardForum/draftList.vue
@@ -2,7 +2,7 @@
  * @Author: lsj a1353474135@163.com
  * @Date: 2023-06-30 14:05:10
  * @LastEditors: lsj a1353474135@163.com
- * @LastEditTime: 2023-07-14 14:01:27
+ * @LastEditTime: 2023-07-24 11:59:43
  * @FilePath: \jichao_app_2\src\pages\cardForum\draftList.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -74,7 +74,9 @@ export default class ClassName extends BaseNode {
         if (this.draftType == "dynamic") {
             app.http.Get(`dataApi/cardCircle/list/me/draft`, this.queryParams, (res: any) => {
                 this.isFetchEnd = res.isFetchEnd
-                let list = res.list || []
+                let list = (res.list || []).filter((item: any) => {
+                    return item.tp != 3
+                })
                 let newList = list.filter((item: any) => {
                     const findIndex: number = this.draftList.findIndex((local: any) => {
                         return local.draftId === item.code
