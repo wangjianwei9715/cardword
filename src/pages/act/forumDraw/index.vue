64f49d817cde694533af7538859893e51949510b
lsj <a1353474135@163.com>
2023-07-26 18:26:24 +0800

    1

--- src/pages/act/forumDraw/index.vue
+++ src/pages/act/forumDraw/index.vue
@@ -2,7 +2,7 @@
  * @Author: lsj a1353474135@163.com
  * @Date: 2023-07-24 17:01:39
  * @LastEditors: lsj a1353474135@163.com
- * @LastEditTime: 2023-07-26 14:39:22
+ * @LastEditTime: 2023-07-26 14:48:03
  * @FilePath: \card-world\src\pages\act\forumDraw\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -295,9 +295,7 @@ export default class ClassName extends BaseNode {
         this.showSwiper = true
     }
     goTuj(){
-        uni.switchTab({
-            url:"/pages/illustration/index"
-        })
+        app.navigateTo.switchTab(1)
     }
     pageJump(url: string) {
         uni.navigateTo({
