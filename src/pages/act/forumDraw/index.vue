de0915af2c835cfd93be434c3262a74a07066a60
lsj <a1353474135@163.com>
2023-07-26 14:34:14 +0800

    调整

--- src/pages/act/forumDraw/index.vue
+++ src/pages/act/forumDraw/index.vue
@@ -2,7 +2,7 @@
  * @Author: lsj a1353474135@163.com
  * @Date: 2023-07-24 17:01:39
  * @LastEditors: lsj a1353474135@163.com
- * @LastEditTime: 2023-07-26 14:23:08
+ * @LastEditTime: 2023-07-26 14:32:11
  * @FilePath: \card-world\src\pages\act\forumDraw\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -623,6 +623,7 @@ export default class ClassName extends BaseNode {
                 color: #626262;
 
                 .red {
+                    margin-left: 2rpx;
                     color: #FA1545;
                 }
             }
