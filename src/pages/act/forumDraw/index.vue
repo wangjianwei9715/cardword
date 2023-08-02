0771e75dc7f660716ed5ee68597a2e8fadf71812
lsj <a1353474135@163.com>
2023-08-02 11:25:17 +0800

    1

--- src/pages/act/forumDraw/index.vue
+++ src/pages/act/forumDraw/index.vue
@@ -2,7 +2,7 @@
  * @Author: lsj a1353474135@163.com
  * @Date: 2023-07-24 17:01:39
  * @LastEditors: lsj a1353474135@163.com
- * @LastEditTime: 2023-07-31 10:27:52
+ * @LastEditTime: 2023-08-02 11:24:44
  * @FilePath: \card-world\src\pages\act\forumDraw\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -221,11 +221,11 @@ export default class ClassName extends BaseNode {
     this.resRewardIds = [];
     this.onDraw = true;
     app.http.Post(`activity/cardCircleDraw/proceed`, {
-      tp: num
+      tp: num==5?2:1
     }, async (res: any) => {
       this.resRewardIds = res.award || [];
       setTimeout(() => {
-        this.reqUserData()
+        this.reqUserData() 
       }, 300)
       for (let i = 0; i < this.resRewardIds.length; i++) {
         await this.startDraw(
