15be6196d7dea3f8ba63594527b5009ac27a65ea
lsj <a1353474135@163.com>
2023-08-07 14:52:56 +0800

    1

--- src/pages/act/forumDraw/index.vue
+++ src/pages/act/forumDraw/index.vue
@@ -2,7 +2,7 @@
  * @Author: lsj a1353474135@163.com
  * @Date: 2023-07-24 17:01:39
  * @LastEditors: lsj a1353474135@163.com
- * @LastEditTime: 2023-08-04 14:34:08
+ * @LastEditTime: 2023-08-04 16:32:10
  * @FilePath: \card-world\src\pages\act\forumDraw\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -787,9 +787,12 @@ export default class ClassName extends BaseNode {
   -webkit-transform: translate(-50%, 0);
   transform: translate(-50%, 0);
   width: inherit;
-
+  display: flex;
+  
+  align-items: center;
+  flex-direction: column;
   image {
-    width: 100%;
+    width: 87%;
     max-height: 100%;
     max-width: 100%;
     overflow-clip-margin: content-box;
@@ -802,7 +805,7 @@ export default class ClassName extends BaseNode {
     font-weight: 600;
     font-family: PingFang SC;
     color: #fff;
-    margin-top: 6rpx;
+    margin-top: 8rpx;
   }
 }
 </style>
