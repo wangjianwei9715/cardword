ddc0d0182e6209f666c0f1684941edb9b99994c2
lsj <a1353474135@163.com>
2023-09-15 11:40:32 +0800

    1

--- src/pages/act/dewu/rule.vue
+++ src/pages/act/dewu/rule.vue
@@ -2,7 +2,7 @@
  * @Author: lsj a1353474135@163.com
  * @Date: 2023-04-27 16:40:21
  * @LastEditors: lsj a1353474135@163.com
- * @LastEditTime: 2023-09-15 11:34:10
+ * @LastEditTime: 2023-09-15 11:39:31
  * @FilePath: \card-world\src\pages\act\dewu\rule.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -10,17 +10,17 @@
     <view class="content">
         <view class="title">活动说明</view>
         <view class="desc">
-            2023.09.15~2023.09.24用户可通过活动页面消耗一定卡币兑换得物免费卡牌评级券(限135pt及以下可用)。两种组合可供兑换。</view>
+            2023.09.15~2023.09.24活动期间，玩家可通过活动页面消耗一定卡币兑换得物免费评级/保护券。每人每天限兑2张，兑换后需当天前往得物app领取，过期作废，效期自领取后14天内有效。得物免费评级/保护券每日12：00补充500张，领完即止。</view>
         <view class="desc">1）得物免费卡牌评级券x1合计1200份，每位用户限兑1次，兑完即止。</view>
         <view class="desc">2）得物免费卡牌评级券x2+卡牌回寄包邮券x1，合计900份，每位用户限兑1次，兑完即止。</view>
-        <view class="desc red">您获得的得物免费卡牌评级券仅限135pt及以下卡片可用</view>
-        <view class="desc" style="margin-top: 10rpx;">
+        <!-- <view class="desc red">您获得的得物免费卡牌评级券仅限135pt及以下卡片可用</view> -->
+        <!-- <view class="desc" style="margin-top: 10rpx;">
             同时在得物app下单5张卡牌评级（得物app内将5笔评级订单合并即可），评级订单完成后额外附赠1张得物免费卡牌评级券(限135pt及以下可用)！下单后，7个工作日内自动发放至得物账户。
-        </view>
+        </view> -->
         <view class="title">使用说明</view>
-        <view class="desc">1）每张评级券可免费评级一张卡片，有效期至2023.10.07，过期作废。</view>
-        <view class="desc">2）用户在卡世界app兑换成功后，通过活动页面专属入口跳转到得物app领取评级券。</view>
-        <view class="desc">3）获得评级券的用户可登录得物app，通过 探索-鉴别服务-评级卡服务或直接在购买页搜索【卡牌评级】 按照操作流程进行评级。</view>
+        <view class="desc">1）每张得物免费评级/保护券可免费评级或保护一张卡片，效期自领取后14天内有效，过期作废。</view>
+        <view class="desc">2）玩家在卡世界app兑换成功后，通过活动页面专属入口跳转至得物app领券，兑换后需在当日领取，隔日重置并作废。</view>
+        <view class="desc">3）获得得物免费评级/保护券的用户可登录得物app，通过 探索-鉴别服务-评级卡服务 按照操作流程进行评级。</view>
         <view class="imgContainer">
             <!-- <image mode="aspectFill" @click="prvImg(index)" v-for="(item, index) in pics" :key="index" :src="item"></image> -->
             <image mode="heightFix" @click="prvImg(0)" src="/static/act/dewu/rule1.png">
