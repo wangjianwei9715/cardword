9925eba9e0c52cedc9b4ea85b2e58d67ba10f2c6
王健伟 <849355894@qq.com>
2023-10-31 15:32:02 +0800

    1

--- src/pages/merchant/utils/util.ts
+++ src/pages/merchant/utils/util.ts
@@ -1,16 +1,10 @@
-/*
- * @FilePath: \jichao_app_2\src\pages\merchant\utils\util.ts
- * @Author: wjw
- * @Date: 2023-10-13 14:56:52
- * @LastEditors: 
- * @LastEditTime: 2023-10-16 13:34:02
- * Copyright: 2023 .
- * @Descripttion: 
- */
 import { app } from "@/app"
-
-// 商家积分和权重
-export function getMerchantIntegral() {
+/**
+ * @func 获取商家积分和权重
+ * @author wjw
+ * @returns Promise
+ */
+export function getMerchantIntegral():Promise<{integral:number;nowMonthWeight:number}> {
     return new Promise((resolve, reject) => {
         app.http.Get("dataApi/merchant/integral/info", {}, (res: any) => {
             resolve(res.data)
