6dc9f6a2b38843dd550325087c6b05a67bb5730b
lsj <a1353474135@163.com>
2023-11-28 11:59:09 +0800

    aspectFill

--- src/pages/act/ibDraw/index.vue
+++ src/pages/act/ibDraw/index.vue
@@ -2,7 +2,7 @@
  * @Author: lsj a1353474135@163.com
  * @Date: 2023-07-24 17:01:39
  * @LastEditors: lsj a1353474135@163.com
- * @LastEditTime: 2023-11-28 11:51:42
+ * @LastEditTime: 2023-11-28 11:58:52
  * @FilePath: \jichao_app_2\src\pages\act\forumDraw\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -28,7 +28,7 @@
     <view class="pageBack"></view>
     <view class="topBanner">
       <view class="hide" :style="{ height: navHeight + 'px' }"></view>
-      <image :src="$parsePic(actInfo.banner || '')"></image>
+      <image mode="aspectFill" :src="$parsePic(actInfo.banner || '')"></image>
     </view>
 
     <view class="machineContainer">
