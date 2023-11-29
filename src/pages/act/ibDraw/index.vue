fd0d658ddf876089325df9ffc50611477faacb06
lsj <a1353474135@163.com>
2023-11-29 17:17:39 +0800

    1

--- src/pages/act/ibDraw/index.vue
+++ src/pages/act/ibDraw/index.vue
@@ -2,7 +2,7 @@
  * @Author: lsj a1353474135@163.com
  * @Date: 2023-07-24 17:01:39
  * @LastEditors: lsj a1353474135@163.com
- * @LastEditTime: 2023-11-29 10:03:34
+ * @LastEditTime: 2023-11-29 17:17:27
  * @FilePath: \jichao_app_2\src\pages\act\forumDraw\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -538,7 +538,7 @@ page {
   background-size: 100% 100%;
   background-image: url("@/static/act/ibDraw/machine.png");
   position: relative;
-  margin-top: -87rpx;
+  margin-top: -17rpx;
   display: flex;
   flex-direction: column;
   align-items: center;
